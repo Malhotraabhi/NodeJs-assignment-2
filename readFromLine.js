758cd51f212afba79b8c7224b4544d15fcdcbe81
abhishek <abhi22525@gmail.com>
2023-07-25 12:24:20 +0530

    First Commit

---
+++ readFromLine.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+input.question('Enter name: ', name => {
+  console.log(`Hello ${name}`);
+  input.close();
+});
